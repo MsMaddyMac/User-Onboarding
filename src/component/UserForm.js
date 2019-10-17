e5bfb9ee3d6aa783bcaad63c00183547082371ab
Maddy <mmcintosh2k14@gmail.com>
2019-10-17 17:17:24 -0400

    adds validation to password and terms of service

--- src/component/UserForm.js
+++ src/component/UserForm.js
@@ -1,15 +1,19 @@
 import React from 'react';
 //import axios from 'axios';
-import { withFormik, Form, Field } from 'formik';
-// import * as Yup from "yup";
+import { withFormik, Form, Field, yupToFormErrors } from 'formik';
+import * as Yup from "yup";
 
-function UserForm({ values }) {
+function UserForm({ values, touched, errors }) {
     return (
         <div className="user-form">
             <Form>
                 <Field type="text" name="name" placeholder="Name" />
                 <Field type="text" name="email" placeholder="Email" />
                 <Field type="text" name="password" placeholder="Password" />
+                {touched.password && errors.password && (
+                    <p className="error">{errors.password}</p>
+                )}
+
 
                 <label className="checkbox-container">
                     Terms of Service
@@ -20,6 +24,7 @@ function UserForm({ values }) {
                     />
                     <span className="checkmark" />
                 </label>
+                {touched.terms && errors.terms && <p className="error">{errors.terms}</p>}
                 <button type="submit">Submit!</button>
             </Form>  
         </div>
@@ -32,9 +37,13 @@ const FormikUserForm = withFormik({
             name: name || "",
             email: email || "",
             password: password || "",
-            terms: terms || ""
+            terms: terms || false
         };
     },
+    validationSchema: Yup.object().shape({
+        password: Yup.string().required('Oops, a password is required!'),
+        terms: Yup.boolean().oneOf([true], 'Agreement of terms is required!')
+    })
 })(UserForm);
 
 
